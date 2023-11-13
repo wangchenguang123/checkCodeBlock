a1d097c410ec3ec93b516382568c1cc32a6a8984
WangChenGuang <1289418375@qq.com>
2023-11-13 16:59:01 +0800

    增加对于增加文件中的函数和函数体提取

--- tree_sitter_parser/analysis_new_file.py
+++ tree_sitter_parser/analysis_new_file.py
@@ -1,10 +1,8 @@
 from tree_sitter_parser.TreeSitterInit import TreeSitterParser
-from tree_sitter_parser.domain import get_all_function_names
+from tree_sitter_parser.domain import get_all_function_names, get_function_body
 from utils.FileUtils import read_file_as_string
 
 so_path = "/datapro/wcg/build/my-languages.so"
-
-
 def check_new_file_type(filepath):
     """
     Check the type of a file based on its extension.
@@ -33,8 +31,21 @@ def get_source_code_ast(source_code, tree_sitter_parser):
         return tree_sitter_parser.parse(bytes(source_code, 'utf-8'))
 
 
+# def get_same_struct_file(source_code,function_name_list):
+
 if __name__ == '__main__':
     source_code = read_file_as_string("/datapro/wcg/resource/s_loongarch.h")
     tree_sitter_parser = check_new_file_type("/datapro/wcg/resource/s_loongarch.h")
     ast_tree = get_source_code_ast(source_code, tree_sitter_parser)
-    print(get_all_function_names(ast_tree.root_node, source_code))
+    function_names = get_all_function_names(ast_tree.root_node, source_code)
+    # # print(get_all_function_definitions(tree_sitter_parser, source_code))
+    # print(function_names)
+    # for fun in function_names:
+    #     print(get_function_body(ast_tree,source_code,fun,"c"))
+
+    origin_same_code = read_file_as_string("/datapro/wcg/resource/s_x86_64.h")
+    tree_sitter_parser_origin = check_new_file_type("/datapro/wcg/resource/s_x86_64.h")
+    ast_tree_origin = get_source_code_ast(origin_same_code, tree_sitter_parser_origin)
+    for fun in function_names:
+        print(get_function_body(ast_tree_origin, origin_same_code, fun, "c"))
+    # print(origin_same_code)
